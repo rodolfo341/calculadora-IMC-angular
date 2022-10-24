f3bef70c747ebb95387eba872eed276fc78d533f
Rodolfo Aravena <rodolfo.aravena.espinosa@gmail.com>
2022-10-24 09:43:12 -0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ResultadoComponent } from './components/resultado/resultado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    ResultadoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/resultado/resultado.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resultado',
+  templateUrl: './resultado.component.html',
+  styleUrls: ['./resultado.component.css']
+})
+export class ResultadoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
