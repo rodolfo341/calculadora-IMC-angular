a355034f7e7206264cf27afcffe889e6c502db34
Rodolfo Aravena <rodolfo.aravena.espinosa@gmail.com>
2022-10-28 19:21:55 -0300

    .

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -9,8 +9,14 @@ export class InicioComponent implements OnInit {
 
     peso: number = 60;
     edad: number = 25;
+    altura: number = 150;
 
     constructor() { }
 
     ngOnInit(): void { }
+
+    cambiarAltura(event: any) {
+        this.altura = event.target.value;
+    }
+
 }
